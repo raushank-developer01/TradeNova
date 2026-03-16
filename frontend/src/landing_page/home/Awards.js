d98be342161a9db1fce8a74151770b7978eb1ce5
Raushan Kumar <raushank.developer@gmail.com>
2026-03-17 01:04:58 +0530

    Update home component

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -4,11 +4,11 @@ function Awards() {
     return ( 
         <div className='container mt-5'>
             <div className='row'>
-                <div className='col-6 p-5 mt-5'>
+                <div className='col-6 p-5'>
                     <img src='media/images/largestBroker.svg'/>
                 </div>
                 <div className='col-6 p-5'>
-                    <h1>Largest stock broker in India</h1>
+                    <h1 className='fs-2'>Largest stock broker in India</h1>
                     <p className='mb-5'>2+ million TreadNova clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:</p>
                     <div className='row'>
                         <div className='col-6'>
