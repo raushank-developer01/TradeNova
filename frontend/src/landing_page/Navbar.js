41abeb0b92ffd9b8d4e5a6f022fdd0186090441f
Raushan Kumar <raushank.developer@gmail.com>
2026-03-18 23:59:40 +0530

    Updated home component

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React from "react";
 
 function Navbar() {
-    return ( 
-        <h1>Navbar </h1>
-     );
+  return (
+    <nav className="navbar navbar-expand-lg border-bottom bg-white">
+      <div className="container">
+
+        {/* Logo */}
+        <a className="navbar-brand fw-bold fs-3" href="#">
+          TradeNova
+        </a>
+
+        {/* Mobile Toggle */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarContent"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Navbar Links */}
+        <div className="collapse navbar-collapse justify-content-end" id="navbarContent">
+          <ul className="navbar-nav gap-4">
+
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Signup</a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link active" href="#">About</a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Product</a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Pricing</a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Support</a>
+            </li>
+
+          </ul>
+        </div>
+
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
 
 function OpenAccount() {
-    return ( 
-        <h1>Open Account</h1>
-     );
+  return (
+    <div className="container p-5 mb-5">
+      <div className="row text-center">
+        <h1 className="mt-5 fs-2">Open a TradeNova Account</h1>
+        <p>
+          Modern platforms and apps, ₹1 investments and flat ₹20 intraday and F&O trades
+        </p>
+        <button
+          className="p-2.5 btn btn-primary fs-5 mb-5"
+          style={{ width: "18%", margin: "0 auto" }}
+        >
+          Sign up Now
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default OpenAccount;
