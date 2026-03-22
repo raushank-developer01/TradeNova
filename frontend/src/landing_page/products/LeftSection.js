454ca861810c6913d7e14b147615dd9b75188d3b
Raushan Kumar <raushank.developer@gmail.com>
2026-03-22 23:01:40 +0530

    Left Section in Product

--- frontend/src/landing_page/products/LeftSection.js
+++ frontend/src/landing_page/products/LeftSection.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
-function LeftSection() {
+function LeftSection({imageURL, productName, productDescription, tryDemo, learnMore, googlePlay, appStore}) {
     return ( 
-        <h1>LeftSection</h1>
+        <div className='container mt-5'>
+            <div className='row'>
+                <div className='col-6'>
+                    <img src={imageURL}/>
+                </div>
+                <div className='col-6 p-5'>
+                    <h1>{productName}</h1>
+                    <p>{productDescription}</p>
+                    <div>
+                        <a href={tryDemo} style={{textDecoration:"None"}}>Try Demo <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                        <a href={learnMore} style={{marginLeft:"75px", textDecoration:"None"}}>Learn More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                    </div>
+                    <div className='mt-3'>
+                        <a href={googlePlay}><img src='media/images/googlePlayBadge.svg'/></a>
+                        <a href={appStore}><img src='media/images/appstoreBadge.svg' style={{marginLeft:"50px"}}/></a>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
