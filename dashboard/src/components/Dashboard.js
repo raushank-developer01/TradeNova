7d2b21c09b8c96a62fddc00a8d5e3899be4d4fe3
Raushan Kumar <raushank.developer@gmail.com>
2026-05-17 15:09:10 +0530

    updated watchlist component

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -12,11 +12,9 @@ import WatchList from "./WatchList";
 
 const Dashboard = () => {
   return (
-    // <div className="dashboard-container">
-    //   <GeneralContextProvider>
-    //     <WatchList />
-    //   </GeneralContextProvider>
-      <div className="content">
+      <div className="dashboard-container">
+        <WatchList />
+        <div className="content">
         <Routes>
           <Route exact path="/" element={<Summary />} />
           <Route path="/orders" element={<Orders />} />
